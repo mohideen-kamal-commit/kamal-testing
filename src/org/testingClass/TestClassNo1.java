07aaba7ddc5d364ebfbfb746afd0daa19a07318c
mohideen-kamal-commit <kamaldeen6359@gmail.com>
2025-03-03 16:31:03 +0530

    testing

---
+++ src/org/testingClass/TestClassNo1.java
@@ -0,0 +1,20 @@
+package org.testingClass;
+
+public class TestClassNo1 {
+	public static void main(String[] args) {
+		int num =0;
+		if (num>0) {
+			System.out.println("Positive Number");
+		
+		}
+		else if (num<0) {
+			System.out.println("Negative Number");
+			
+		}
+		else {
+			System.out.println("Netural Number");
+
+		}
+			
+		}
+}
